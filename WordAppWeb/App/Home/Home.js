6feee9860c40a3429e1cd636919c8cc59ae9cd86
Astrid Hackenberg <astridha@microsoft.com>
2014-10-16 09:57:12 +0200

    Initial commit

---
+++ WordAppWeb/App/Home/Home.js
@@ -0,0 +1,94 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            var viewModel = {
+                patients: ko.observable(),
+                getPatientData: function () {
+                    $.ajax({
+                        url: '../../api/Patient/' + this.Id(),
+                        type: 'GET'
+                    }).done(function (data) {
+                       
+
+
+
+                    });
+                }
+            };
+
+
+            //Get json data
+            $.ajax({
+                url: '../../api/Patient',
+                type: 'GET'
+            }
+                ).done(function (data) {
+
+                    viewModel.patients = ko.mapping.fromJS(data);
+
+                    ko.applyBindings(viewModel, document.getElementById("content-main"));
+
+                }).fail(function (status) {
+                    console.log(status);
+                });
+
+        });
+    };
+
+      function addAndBindControl() {
+
+        Office.context.document.bindings.addFromNamedItemAsync("MyContentControlTitle", "text", { id: 'myBinding' }, function (result) {
+
+            if (result.status == "failed") {
+
+                if (result.error.message == "The named item does not exist.")
+                    var myOOXMLRequest = new XMLHttpRequest();
+                    var myXML;
+                    myOOXMLRequest.open('GET', '../../Snippets_BindAndPopulate/ContentControl.xml', false);
+                    myOOXMLRequest.send();
+
+                    if (myOOXMLRequest.status === 200) {
+                        myXML = myOOXMLRequest.responseText;
+                    }
+                    Office.context.document.setSelectedDataAsync(myXML, { coercionType: 'ooxml' }, function (result) {
+                        Office.context.document.bindings.addFromNamedItemAsync("MyContentControlTitle", "text", { id: 'myBinding' });
+                    });
+            }
+            });
+        }
+    
+    //add content at bound location
+    function populateBinding(filename) {
+
+        var myOOXMLRequest = new XMLHttpRequest();
+        var myXML;
+        myOOXMLRequest.open('GET', filename, false);
+            myOOXMLRequest.send();
+            if (myOOXMLRequest.status === 200) {
+                myXML = myOOXMLRequest.responseText;
+            }
+            Office.select("bindings#myBinding").setDataAsync(myXML, { coercionType: 'ooxml' });
+        };
+
+
+
+    // Reads data from current document selection and displays a notification
+    function getDataFromSelection() {
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    app.showNotification('The selected text is:', '"' + result.value + '"');
+                } else {
+                    app.showNotification('Error:', result.error.message);
+                }
+            }
+        );
+    }
+})();
---
+++ WordAppWeb/Controllers/PatientController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WordAppWeb.Controllers
+{
+    public class PatientController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Patient> Get()
+        {
+            return new List<Patient>  { new Patient{Name="Patient1", Id = 1}, new Patient{Name = "Patient2", Id=2}};
+        }
+
+        // GET api/<controller>/5
+        public Patient Get(int id)
+        {
+            return new Patient { Name = "FoundPAtient" , Id = 3, Medicine = "Morfine"};
+        }
+
+      
+    }
+
+    public class Patient
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public string Medicine { get; set; }
+    }
+}
